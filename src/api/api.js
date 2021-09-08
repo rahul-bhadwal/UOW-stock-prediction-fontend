dd690a03206ef7959d473dd8a553acdfbd7e4387
Akhil Ramani <ramaniakhil094@gmail.com>
2021-09-08 21:10:31 +1000

    news api updated

--- src/api/api.js
+++ src/api/api.js
@@ -3,4 +3,4 @@ import { NEWS_API_KEY, STOCK_API_KEY } from '../constants/common'
 
 export const fetchStockData = () => axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=TLSYY&outputsize=full&apikey=${STOCK_API_KEY}`)
 
-export const getNews = queryText => axios.get(`https://newsapi.org/v2/everything?q=${queryText}&apiKey=${NEWS_API_KEY}`)
+export const getNews = queryText => axios.get(`https://uniproject-test.netlify.app/news?q=${queryText}`)
